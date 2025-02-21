e69530a0d0e9f73f17df94e2e7cb6566f19d16d2
Ahmedz100 <aptechahmed21@gmail.com>
2025-02-21 18:19:37 +0500

    add data

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
+
+const User = require('./Model/User');
 // require('dotenv').config();
 const mongoose = require('mongoose');
-
+let UserData = async function (){
+    try {
+    await User.create({
+         name: "John Doe b",
+         email: "johndoe@example.com",
+         password: "password123",
+     }).then(()=>{
+         console.log("User created successfully")
+     }).catch((e)=>{
+         console.log(e)
+     })
+    } catch (error) {
+     console.log(error)
+    }
+ }
 app.get("/", (req, res) => {
     res.send("Home");
 })
@@ -11,6 +27,7 @@ app.get("/", (req, res) => {
 mongoose.connect("mongodb+srv://admin:admin123@clusteraptech.sw28l.mongodb.net/merndb?retryWrites=true&w=majority&appName=Clusteraptech")
 .then(()=>{
     app.listen(3005, ()=> {
+        UserData();
         console.log("Server Start")
     });
 }).catch((error)=>{
