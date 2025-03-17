dd0c2fed074b9f5ea4c7b19267955d46af015f00
samreenrafiq2796 <samreenrafiq@aptechnorth.edu.pk>
2025-03-17 16:03:28 +0500

    update sidebar

--- my-app/src/component/userpanel/Upsidebar.jsx
+++ my-app/src/component/userpanel/Upsidebar.jsx
@@ -28,9 +28,7 @@ const Upsidebar = () => {
                 <hr class="sidebar-divider" />
 
                 {/* <!-- Heading --> */}
-                <div class="sidebar-heading">
-                    Interface
-                </div>
+                
 
                 {/* <!-- Nav Item - Pages Collapse Menu --> */}
                 <li class="nav-item">
@@ -41,29 +39,12 @@ const Upsidebar = () => {
                     </a>
                     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                         <div class="bg-white py-2 collapse-inner rounded">
-                            <h6 class="collapse-header">Custom Components:</h6>
+                        
                             <Link class="collapse-item" to="upaddworkout">Add</Link>
                             <Link class="collapse-item" to="upshowworkout">Show</Link>
                         </div>
                     </div>
                 </li>
-
-
-
-                {/* <li class="nav-item">
-  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-    aria-expanded="true" aria-controls="collapseTwo">
-    <i class="fas fa-fw fa-cog"></i>
-    <span>Activity Notifications</span>
-  </a>
-  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-    <div class="bg-white py-2 collapse-inner rounded">
-      <h6 class="collapse-header">Custom Components:</h6>
-      <a class="collapse-item" href="buttons.html">Add</a>
-      <a class="collapse-item" href="cards.html">Show</a>
-    </div>
-  </div>
-</li> */}
 
 
 
@@ -78,7 +59,7 @@ const Upsidebar = () => {
                     <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                         data-parent="#accordionSidebar">
                         <div class="bg-white py-2 collapse-inner rounded">
-                            <h6 class="collapse-header">Custom Utilities:</h6>
+                       
                             <Link class="collapse-item" to="Upnutrition">Add</Link>
                             <Link class="collapse-item" to="Upnutritionshow">Show</Link>
                         </div>
