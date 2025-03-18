5ad4bf97e7c2a6ec8660be6a7169bde03211f72a
samreenrafiq2796 <samreenrafiq@aptechnorth.edu.pk>
2025-03-18 14:57:48 +0500

    remove extra option

--- my-app/src/admin/component/Navbar.jsx
+++ my-app/src/admin/component/Navbar.jsx
@@ -183,18 +183,7 @@ const Navbar = () => {
                         {/* <!-- Dropdown - User Information --> */}
                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                             aria-labelledby="userDropdown">
-                            <a class="dropdown-item" href="#">
-                                <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Profile
-                            </a>
-                            <a class="dropdown-item" href="#">
-                                <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Settings
-                            </a>
-                            <a class="dropdown-item" href="#">
-                                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Activity Log
-                            </a>
+                          
                             {/* data-toggle="modal" data-target="#logoutModal" */}
                             <div class="dropdown-divider"></div>
                             <button class="dropdown-item" onClick={logout}>
